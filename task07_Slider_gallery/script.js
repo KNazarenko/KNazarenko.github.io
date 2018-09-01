e384f7d91bde3b808dd29f278d8587f4e07c5718
knazarenko <nko@ukr.net>
2018-09-01 17:36:37 +0300

    task07_changed ClassName to ClassList

--- task07_Slider_gallery/script.js
+++ task07_Slider_gallery/script.js
@@ -4,18 +4,20 @@ let btn_next = document.querySelector(".next");
 let images = document.querySelectorAll(".photo img");
 let i = 0;
 
+//by add/remove
 btn_prev.onclick = function() {
-  images[i].className = " ";
+  images[i].classList.remove("showed");
   i--;
   if (i < 0) i = images.length - 1;
-  images[i].className = "showed";
+  images[i].classList.add("showed");
 };
 
+//by toggle
 btn_next.onclick = function() {
-  images[i].className = " ";
+  images[i].classList.toggle("showed");
   i++;
   if (i >= images.length) {
     i = 0;
   }
-  images[i].className = "showed";
+  images[i].classList.toggle("showed");
 };
