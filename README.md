2cf2c30c53ca196f8c3d2919f08e140906f7db91
knazarenko <nko@ukr.net>
2018-09-03 00:51:09 +0300

    class Slider

--- README.md
+++ README.md
@@ -8,8 +8,6 @@
 
 #### [task 05 - DAISY](https://knazarenko.github.io/task05_daisy/) (css only)
 
-#### [task 05 - daisy - full screen](https://knazarenko.github.io/task05_daisy%20-%20full%20screen/)
-
 #### [task 06 - LUCID](https://knazarenko.github.io/task06_LUCID_adaptive_layout/dist/index.html) (адаптивная верстка, [Snazzy Maps](https://snazzymaps.com/))
 
-#### [task 07 - Slider_gallery](https://knazarenko.github.io/task07_Slider_gallery/) (js, bootstrap-buttons)
+#### [task 07 - Slider ООП](https://knazarenko.github.io/task07_Slider_gallery/) (класс Slider на чистом js, bootstrap-buttons)
--- task07_Slider_gallery/script.js
+++ task07_Slider_gallery/script.js
@@ -1,23 +1,49 @@
-let btn_prev = document.querySelector(".prev");
-let btn_next = document.querySelector(".next");
+let slider1 = new Slider({
+  images: ".gallery_1 img",
+  btnPrev: ".gallery_1 .prev",
+  btnNext: ".gallery_1 .next",
+  auto: false
+});
 
-let images = document.querySelectorAll(".photo img");
-let i = 0;
+let slider2 = new Slider({
+  images: ".gallery_2 img",
+  auto: true,
+  rate: 1500
+});
 
-//by add/remove
-btn_prev.onclick = function() {
-  images[i].classList.remove("showed");
-  i--;
-  if (i < 0) i = images.length - 1;
-  images[i].classList.add("showed");
-};
+function Slider(obj) {
+  this.imgs = document.querySelectorAll(obj.images);
+  this.btnPrev = document.querySelector(obj.btnPrev) || {}; //if there are no buttons
+  this.btnNext = document.querySelector(obj.btnNext) || {}; //if there are no buttons
+  this.auto = obj.auto;
+  this.rate = obj.rate || 1000;
+  let slider = this;
+  let i = 0;
 
-//by toggle
-btn_next.onclick = function() {
-  images[i].classList.toggle("showed");
-  i++;
-  if (i >= images.length) {
-    i = 0;
+  //by add/remove
+  this.prev = function() {
+    console.log(1);
+    slider.imgs[i].classList.remove("showed");
+    i--;
+    if (i < 0) i = slider.imgs.length - 1;
+    slider.imgs[i].classList.add("showed");
+  };
+  this.btnPrev.onclick = this.prev;
+
+  //by toggle
+  this.next = function() {
+    slider.imgs[i].classList.toggle("showed");
+    i++;
+    if (i >= slider.imgs.length) i = 0;
+    slider.imgs[i].classList.toggle("showed");
+  };
+  this.btnNext.onclick = this.next;
+
+  if (slider.auto) {
+    setInterval(slider.next, slider.rate);
   }
-  images[i].classList.toggle("showed");
-};
+}
+
+// setInterval(function() {
+//   slider2.next();
+// }, 1500);
