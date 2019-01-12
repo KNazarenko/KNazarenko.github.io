19a795fde2260a43ce365eb3885be43591169e93
knazarenko <nko@ukr.net>
2019-01-12 19:33:43 +0200

    removed task11

--- task11_todo_react-redux/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import flagReducer from './flagReducer';
-import taskReducer from './taskReducer';
-
-export default combineReducers({
-  tasks: taskReducer,
-  flag: flagReducer
-});
